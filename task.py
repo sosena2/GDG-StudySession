6792fffbb0a3901f8b3cc001cab450760e123ed8
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-12-25 12:00:07 +0300

    update task.py on branch question4

--- task.py
+++ task.py
@@ -1,4 +1,10 @@
-#function to find the largest number in a list
-def findLargest(nums:list[int]):
-    maxValue=max(nums)
-    return maxValue
+#function to count the frequency of word in a sentence
+def word_frequency(sentence):
+    words = sentence.split()
+    frequency = {}
+    for word in words:
+        word = word.lower()
+        frequency[word] = frequency.get(word, 0) + 1
+    return frequency
+
+
